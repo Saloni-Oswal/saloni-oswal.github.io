b0ede08d4b565c14d524dc7544dd21a3166ecaa9
Saloni Oswal <salonioswal22@gmail.com>
2024-03-21 01:58:30 -0400

    Update scale.fix.js

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -6,7 +6,7 @@
        mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
        mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
        
-       $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(29, 78, 216, 0.15),  transparent 80%)');
+       $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(29, 78, 216, 0.15), transparent 80%)');
      });
      
     var metas = document.getElementsByTagName('meta'),
